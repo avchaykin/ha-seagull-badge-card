0282eb04264d393cd03fd19ec5b343e7de1f6210
Chaykin <chay@Mac-002.lan>
2026-04-02 20:56:17 +0200

    Update editor metadata to current commit hash

--- seagull-badges-card.js
+++ seagull-badges-card.js
@@ -1,5 +1,5 @@
 const SEAGULL_BADGES_CARD_VERSION = "0.1.2-dev";
-const SEAGULL_BADGES_CARD_COMMIT = "pending";
+const SEAGULL_BADGES_CARD_COMMIT = "ca808b5";
 
 class SeagullBadgesCard extends HTMLElement {
   static getStubConfig() {
